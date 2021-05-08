a3f80e94966531b5af2fcc4dbd97255805a556d0
Klelver <klelvercz@hotmail.com>
2021-05-07 22:33:10 -0300

    Adicionando metodo main

--- src/br/edu/fafic/es/main/App.java
+++ src/br/edu/fafic/es/main/App.java
@@ -1,5 +1,8 @@
 package br.edu.fafic.es.main;
 
 public class App {
-    
+    public static void main(String[] args) {
+
+        System.out.println("Projeto executado com sucesso!!!");
+    }
 }
--- src/br/edu/fafic/es/main/Subtracao.java
+++ src/br/edu/fafic/es/main/Subtracao.java
@@ -2,7 +2,5 @@ package br.edu.fafic.es.main;
 
 public class Subtracao {
 
-    public void menssagem (){
-        System.out.println("Apaguei o método de subtrair!!!");
-    }
+    
 }
